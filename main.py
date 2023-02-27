4d031b9162b60b84a5d2d0dca0c78009d691d097
AlejandroFNadal <alexandronadal@gmail.com>
2023-02-27 15:15:06 +0100

    WordAcc checks

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from utils.parameters import get_parameters, param_dict_to_string
 from utils.logger import printv
 from PIL import Image
 from unidecode import unidecode
+from tqdm import tqdm
 
 # arguments for command line: input_folder, text_folder, log_file_name (optional), verbose (optional), parameter_file (optional), measure_only (optional)
 parser = argparse.ArgumentParser(description='Accuracy measurement and hyperparametrization of tesseract-ocr.')
@@ -35,14 +36,14 @@ def measure_accuracy(input_folder, text_folder, output_folder, log_file_name, ve
     printv('Path to input folder: ' + input_folder, True, verbose)
     printv('Path to text folder: ' + text_folder, True, verbose)
     printv('Path to output folder: ' + output_folder, True, verbose)
-
+    # print parameters to log file
+    with open(log_file_name, 'w') as log_file:
+        log_file.write('Parameters: ' + string_parameters + '\n')
     input_images = os.listdir(input_folder)
-    print(os.path.join(input_folder, input_images[0]))
     # iterate over the images
-    for image in input_images:
+    for image in tqdm(input_images):
         # get the text of the image
         binary_image = Image.open(os.path.join(input_folder, image))
-        print(binary_image)
         text = pytesseract.image_to_string(binary_image, lang=parameters['lang'])
         if args.remove_new_line:
             text = text.replace('\n', ' ')
@@ -55,6 +56,29 @@ def measure_accuracy(input_folder, text_folder, output_folder, log_file_name, ve
         # save the text in the text_folder
         with open(os.path.join(output_folder, text_file_name), 'w') as f:
             f.write(text)
+    # now, we iterate again. For evey ile in the output_folder, we get the text and use the system utility wordacc to measure the file diference with 
+    # the text in the text_folder
+    # wordacc has the following invocation format wordacc correct_file file_to_check
+    # from the output, we will extract the number to the left of the word 'Accuracy'
+    average_accuracy = 0
+    for image in tqdm(input_images):
+        text_file_name = image.split('.')[0] + '.txt'
+        correct_file = os.path.join(text_folder, text_file_name)
+        file_to_check = os.path.join(output_folder, text_file_name)
+        accuracy = os.popen('wordacc ' + correct_file + ' ' + file_to_check).read()
+        accuracy = accuracy.split('\n')[4] # this is due to the output format
+        # the tool does not return the actual value...
+        accuracy = accuracy.split('%')[0]
+        accuracy = float(accuracy)
+        # save the accuracy in the log file
+        with open(log_file_name, 'a') as f:
+            f.write(image + ' ' + str(accuracy) + '\n')
+        average_accuracy += accuracy
+    average_accuracy = average_accuracy / len(input_images)
+    with open(log_file_name, 'a') as f:
+        f.write('Average accuracy: ' + str(average_accuracy) + '\n')
+    with open(log_file_name, 'r') as f:
+        print(f.read())
 
 if __name__ == '__main__':
     if args.measure_only:
